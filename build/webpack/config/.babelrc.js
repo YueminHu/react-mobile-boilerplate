481b262e09292cae68ec7e74778544f2c78aa34e
Yuemin.Hu <hym920408@gmail.com>
2019-06-18 17:33:40 +0800

    update deps

--- build/webpack/config/.babelrc.js
+++ build/webpack/config/.babelrc.js
@@ -3,17 +3,17 @@ module.exports = {
   babelrc: false,
   presets: [
     [
-      "@babel/preset-env",
-      { targets: { browsers: "last 2 versions" } } // or whatever your project requires
+      '@babel/preset-env',
+      { targets: { browsers: 'last 2 versions' } } // or whatever your project requires
     ],
-    "@babel/preset-typescript",
-    "@babel/preset-react"
+    '@babel/preset-typescript',
+    '@babel/preset-react'
   ],
   plugins: [
     // plugin-proposal-decorators is only needed if you're using experimental decorators in TypeScript
-    "@babel/plugin-syntax-dynamic-import",
-    ["@babel/plugin-proposal-decorators", { legacy: true }],
-    ["@babel/plugin-proposal-class-properties", { loose: true }],
-    "react-hot-loader/babel"
+    '@babel/plugin-syntax-dynamic-import',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    'react-hot-loader/babel'
   ]
 };
